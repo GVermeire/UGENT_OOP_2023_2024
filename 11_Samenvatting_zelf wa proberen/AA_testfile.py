8feb26c64f95a9298737ebcad51619369c9833ed
Vermeire <132931623+GVermeire@users.noreply.github.com>
2024-01-01 01:23:42 +0100

    Delete 11_Samenvatting_zelf wa proberen directory

--- 11_Samenvatting_zelf wa proberen/AA_testfile.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-d = datetime.now()
-print("Current year is " + str(d.year))
-print("Current month is " + str(d.month))
-print("Current day of month is " + str(d.day))
-print("Current hour is " + str(d.hour))
-print("Current minute is " + str(d.minute))
-print("Current second is " + str(d.second))
